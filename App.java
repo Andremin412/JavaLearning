4cc84c4fe666bbc1ba80c89ba3128d0f592d8ba6
Andremin412 <170220709+Andremin412@users.noreply.github.com>
2024-11-05 12:12:02 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double tiger_A = 180.0;
+        double tiger_B = 200.0;
+        System.out.println("The bigger one is:" + (tiger_A >= tiger_B?"tiger_A":"tiger_B"));
+    }
+}
